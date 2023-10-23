eaf5a160102d67aef43ac4149a1eda1521a6496d
Aarifansarikht <86506816+Aarifansarikht@users.noreply.github.com>
2023-10-22 22:32:32 -0700

    Footer

--- src/components/common/footer/Footer.js
+++ src/components/common/footer/Footer.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import Styles from './style.module.scss'
-import FooterBack from '../../../assets/vector/footer-back.svg'
+import FooterLeftCorner from '../../../assets/vector/footer-left-corner.svg'
+import FooterRightCorner from '../../../assets/vector/footer-right.svg'
+
+
 export default function Footer(props) {
     return (
         <section className={Styles.FooterSection}>
             <div className={Styles.Container}>
                 <div className={Styles.FooterContent}>
-                    <div className={Styles.FooterTop}>
+                    <div className={Styles.FooterMain}>
 
-                    </div>
-                    <div className={Styles.FooterBottom}>
+                        <div className={Styles.FooterTop}>
 
+                        </div>
+                        <div className={Styles.FooterBottom}>
+
+                        </div>
                     </div>
+                    <img src={FooterLeftCorner} className={Styles.FooterLeftCorner} />
+                    <img src={FooterRightCorner} className={Styles.FooterRightCorner} />
+
                 </div>
             </div>
         </section>
